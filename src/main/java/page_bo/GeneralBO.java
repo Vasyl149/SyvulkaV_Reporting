86310659919544056c924696a334a05064610594
vasl <syvulkav@gmail.com>
2022-02-23 22:50:01 +0200

    Initial commit

---
+++ src/main/java/page_bo/GeneralBO.java
@@ -0,0 +1,9 @@
+package page_bo;
+
+import org.apache.log4j.Logger;
+import page_bo.main_pages.commonBO.MainMenuBO;
+
+public class GeneralBO {
+    protected static final Logger logger = Logger.getLogger(MainMenuBO.class);
+
+}
---
+++ src/test/java/LeftMenuTest.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.Test;
+import page_bo.main_pages.InboxPageBO;
+import page_bo.main_pages.commonBO.MainMenuBO;
+
+import java.io.UnsupportedEncodingException;
+
+public class LeftMenuTest extends BaseTest {
+    @Test
+    public void menuListTest2() throws InterruptedException, UnsupportedEncodingException {
+
+        int unreadLetters = new InboxPageBO().
+                verifyUnreadLettersPresent().
+                getUnreadLettersNumber();
+
+        new MainMenuBO().verifyInboxIsSelected()
+                .verifyNavigationMenu().
+                verifyUnreadLettersNumber(unreadLetters);
+
+    }
+
+}
