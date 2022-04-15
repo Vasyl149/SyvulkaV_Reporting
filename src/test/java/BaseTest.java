22a6ee0e9551d5e8ad4853d2ea5a8aca22b4890a
vasl <syvulkav@gmail.com>
2022-04-15 19:14:40 +0300

    fixed2

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import page_bo.main_pages.commonBO.MainMenuBO;
 import page_bo.main_pages.loginPageBO.LoginPageBO;
 
@@ -17,7 +18,7 @@ public abstract class BaseTest {
     private final String browser = System.getProperty("environment");
 
     @BeforeMethod
-    //@Parameters("browser")
+//    @Parameters("browser")
     public void preCondition() {
         WebDriver driver = new BrowserInstance().createInstance(browser);
         DriverManager.setDriver(driver);
